b97ec13a1db4409d275e46635608714ec247ba8e
assafvan <vanunuassaf@gmail.com>
2021-12-01 14:34:57 +0200

    multiplication_table

--- JavaScript/theDom/snake/snake.js
+++ JavaScript/theDom/snake/snake.js
@@ -1,6 +1,6 @@
 import { getInputDirection } from "./input.js";
 
-export const snakeSpeed = 1;
+export let snakeSpeed = 1;
 const snakeBody = [{ x: 11, y: 11 }];
 let newSegments = 0;
 
@@ -25,6 +25,7 @@ export function draw(gameBoard) {
 
 export function expandSnake(amount) {
   newSegments += amount;
+  snakeSpeed++;
 }
 
 export function onSnake(position, { ignoreHead = false } = {}) {
