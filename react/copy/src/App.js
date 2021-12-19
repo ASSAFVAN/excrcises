bb5a157f2226e21d04452c1238abee4cbec43b73
assafvan <vanunuassaf@gmail.com>
2021-12-19 14:46:43 +0200

    12.1

--- react/copy/src/App.js
+++ react/copy/src/App.js
@@ -1,26 +1,12 @@
 import React from "react";
 import "./App.css";
-import CustomButton from "./Components/CustomButton";
+import Button from "./Components/Button";
 
 class App extends React.Component {
-  state = { color: "" };
-  displayButtons = () => {
-    const colors = ["blue", "red", "yellow"];
-    return colors.map((color) => {
-      return <CustomButton text={color} parentCallback={this.handleCallback} />;
-    });
-  };
-
-  handleCallback = (childData) => {
-    this.setState({ color: childData });
-  };
-
   render() {
-    const { color } = this.state;
     return (
       <div>
-        {this.displayButtons()}
-        <div>The color selected is:{this.state.color}</div>
+        <Button />
       </div>
     );
   }
