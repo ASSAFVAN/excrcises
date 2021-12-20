c45f6b149d96102155e54c5589b7bd3fd2272d3b
assafvan <vanunuassaf@gmail.com>
2021-12-20 14:11:26 +0200

    14.1

--- react/copy/src/App.js
+++ react/copy/src/App.js
@@ -1,35 +1,14 @@
 import React from "react";
 import "./App.css";
-import { data } from "./data";
-import Card from "./Components/Card";
-import Name from "./Components/Name";
+import InputText from "./Components/InputText";
 
 class App extends React.Component {
-  state = { names: [], older: [] };
-
-  componentDidMount() {
-    const namesArr = data.map((user) => {
-      return user.name;
-    });
-    const olderArr = data.filter((obj) => {
-      const year = obj.birthday.slice(-4);
-      return year < 1990;
-    });
-    // console.log(olderArr);
-    this.setState({ names: namesArr, older: olderArr });
-  }
+  state = { textInput: "" };
 
   render() {
-    console.log(this.state);
-
     return (
       <div>
-        <div>
-          <Name names={this.state.names} />
-          <div>
-            {this.state.older.length && <Card names={this.state.older} />}
-          </div>
-        </div>
+        <InputText />
       </div>
     );
   }
