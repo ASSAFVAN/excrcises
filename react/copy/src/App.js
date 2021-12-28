74464a393fb1ea95a37560a4bda3d1c5f02892aa
assafvan <vanunuassaf@gmail.com>
2021-12-28 16:16:52 +0200

    21.1

--- react/copy/src/App.js
+++ react/copy/src/App.js
@@ -1,19 +1,33 @@
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
 import "./App.css";
-import FetchData from "./Components/FetchData";
 
 export default function App() {
   const [show, setShow] = useState(false);
+  const label = useRef("Edit");
+  const inputRef = useRef(null);
+
+  useEffect(() => {
+    if (inputRef.current) {
+      inputRef.current.focus();
+    }
+  }, [show]);
+
+  const handleClick = () => {
+    if (label.current === "Edit") {
+      label.current = "Save";
+      setShow(true);
+    } else {
+      label.current = "Edit";
+      setShow(false);
+    }
+  };
+
   return (
-    <div className="App">
-      <button
-        onClick={() => {
-          setShow(!show);
-        }}
-      >
-        toggle
+    <div>
+      {show && <input ref={inputRef} type="text" />}
+      <button ref={label} onClick={handleClick}>
+        {label.current}
       </button>
-      {show && <FetchData />}
     </div>
   );
 }
