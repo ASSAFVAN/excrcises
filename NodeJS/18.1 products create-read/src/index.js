f6d9e92b3df512ca7c38634d72e162ff5f8d832d
assafvan <vanunuassaf@gmail.com>
2022-01-19 13:20:24 +0200

    20.1 update and delete

--- NodeJS/18.1 products create-read/src/index.js
+++ NodeJS/18.1 products create-read/src/index.js
@@ -98,6 +98,67 @@ app.get("/products/:id", async (req, res) => {
   // });
 });
 
+app.patch("/products/:id", async (req, res) => {
+  const updates = Object.keys(req.body);
+  const allowUpdates = [
+    "name",
+    "category",
+    "isActive",
+    "details.description",
+    "details.price",
+    "details.discount",
+    "details.images",
+    "details.phoneNumber",
+    "dateAdded",
+  ];
+  const isValidOperation = updates.every((item) => {
+    return allowUpdates.includes(item);
+  });
+  if (!isValidOperation) {
+    return res.status(400).send({ error: "invalid updates" });
+  }
+  try {
+    const updatedProduct = await Product.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      {
+        new: true,
+        runValidators: true,
+      }
+    );
+    if (!updatedProduct) {
+      res.status(404).send({ error: "no such product to update" });
+    }
+    res.status(200).send(updatedProduct);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
+
+app.delete("/products", async (req, res) => {
+  try {
+    const products = await Product.deleteMany({});
+    if (!products) {
+      res.status(404).send({ error: "no products to delete" });
+    }
+    res.status(200).send(products);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
+
+app.delete("/products/:id", async (req, res) => {
+  try {
+    const productToDelete = await Product.findOneAndDelete(req.params.id);
+    if (!productToDelete) {
+      res.status(404).send({ error: "no such product to delete" });
+    }
+    res.status(200).send(productToDelete);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
+
 app.listen(port, () => {
   console.log("Server is up on port " + port);
 });
