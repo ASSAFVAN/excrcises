1e481aadefa97de199cb51df0d959aca369b7c26
assafvan <vanunuassaf@gmail.com>
2022-01-19 11:19:23 +0200

    19.1 async

--- NodeJS/18.1 products create-read/src/index.js
+++ NodeJS/18.1 products create-read/src/index.js
@@ -7,69 +7,95 @@ const port = process.env.PORT || 5000;
 
 app.use(express.json());
 
-app.post("/products", (req, res) => {
+app.post("/products", async (req, res) => {
   const product = new Product(req.body);
-
-  product
-    .save()
-    .then(() => {
-      res.status(201).send(product);
-    })
-    .catch((e) => {
-      res.status(400).send(e);
-    });
+  try {
+    await product.save();
+    res.status(201).send(product);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+  // .then(() => {
+  //   res.status(201).send(product);
+  // })
+  // .catch((e) => {
+  //   res.status(400).send(e);
+  // });
 });
 
-app.get("/products", (req, res) => {
-  Product.find({})
-    .then((products) => {
-      res.send(products);
-    })
-    .catch((e) => {
-      res.status(500).send();
-    });
+app.get("/products", async (req, res) => {
+  try {
+    const products = await Product.find({});
+    res.status(200).send(products);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+  // .then((products) => {
+  //   res.send(products);
+  // })
+  // .catch((e) => {
+  //   res.status(500).send();
+  // });
 });
 
-app.get("/products/active", (req, res) => {
-  Product.find({ isActive: true })
-    .then((products) => {
-      if (!products) {
-        return res.status(404).send();
-      }
-      res.send(products);
-    })
-    .catch((e) => {
-      res.status(500).send();
-    });
+app.get("/products/active", async (req, res) => {
+  const activeProducts = await Product.find({ isActive: true });
+  try {
+    res.status(200).send(activeProducts);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+  // .then((products) => {
+  //   if (!products) {
+  //     return res.status(404).send();
+  //   }
+  //   res.send(products);
+  // })
+  // .catch((e) => {
+  //   res.status(500).send();
+  // });
 });
 
-app.get("/products/range", (req, res) => {
+app.get("/products/range", async (req, res) => {
   const minValue = req.query.min;
   const maxValue = req.query.max;
-  Product.find({ "details.price": { $gte: minValue, $lte: maxValue } })
-    .then((product) => {
-      if (!product) {
-        return res.status(404).send();
-      }
-      res.send(product);
-    })
-    .catch((e) => {
-      res.status(500).send();
-    });
+  const rangeProducts = await Product.find({
+    "details.price": { $gte: minValue, $lte: maxValue },
+  });
+  try {
+    res.status(200).send(rangeProducts);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+
+  // .then((product) => {
+  //   if (!product) {
+  //     return res.status(404).send();
+  //   }
+  //   res.send(product);
+  // })
+  // .catch((e) => {
+  //   res.status(500).send();
+  // });
 });
 
-app.get("/products/:id", (req, res) => {
+app.get("/products/:id", async (req, res) => {
   const _id = req.params.id;
-  Product.findById(_id)
-    .then((product) => {
-      if (!product) {
-        return res.status(404).send();
-      }
-      res.send(product);
-    })
-    .catch((e) => {
-      res.status(500).send();
-    });
+  const pid = await Product.findById(_id);
+  try {
+    res.status(200).send(pid);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+  // .then((product) => {
+  //   if (!product) {
+  //     return res.status(404).send();
+  //   }
+  //   res.send(product);
+  // })
+  // .catch((e) => {
+  //   res.status(500).send();
+  // });
 });
 
 app.listen(port, () => {
